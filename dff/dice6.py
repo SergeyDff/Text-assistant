e4475a680e0e72caf01d1a479665e77f46a599ba
dff <130085050+SergeyDff@users.noreply.github.com>
2023-05-18 20:11:15 +0300

    Update dice6.py

--- dff/dice6.py
+++ dff/dice6.py
@@ -1,28 +1,34 @@
-def amount():
-    a = '106'
-    b = '15'
-    if len(a) > len(b):
-        for v in range(len(a) - len(b)):
-            b = '0' + b
-
-    if len(a) < len(b):
-        for v in range(len(b) - len(a)):
-            a = '0' + a
-
-    print(a)
-    print(b)
-    result = ''
-    amount_fn_sn = 0
-    for i in range(len(a)):
-        amount_fn_sn = int(a[i]) + int(b[i])
-        if amount_fn_sn >= 10:
-            list_result = list(result)
-            list_result[i-1] = str(int(list_result[i-1]) + amount_fn_sn // 10)
-            list_result.append(str(amount_fn_sn % 10))
-            result = ''.join(list_result)
-        else:
-            result = result + str(amount_fn_sn)
-    print(result)
+import random
 
 
-amount()
+class Person:
+    def __init__(self, name, room) -> None:
+        self.name = name
+        self.room = room
+
+    def __del__(self):
+        print(f'{self.name} умер в {self.room}')
+
+    def __str__(self) -> str:
+        return f'{self.name} находится в {self.room}'
+
+    def __lt__(self, other: 'Person'):
+        return self.room == other.room
+
+
+persons = [
+    Person('Валера', 'гостинной'),
+    Person('Сергей', 'спальне'),
+    Person('Санек', 'столовой')
+]
+
+rooms = ['кухне', 'спальне', 'гостинной', 'столовой']
+a = ''
+while len(persons) != 1:
+    for i in range(len(persons) - 1):
+        persons[i].room = random.choice(rooms)
+        print(persons[i])
+        if persons[i].room == a:
+            del persons[i]
+        a = persons[i].room
+        input()
