38073e3891a21ace71adb9dc3c72357a9da5b843
dff <130085050+SergeyDff@users.noreply.github.com>
2023-06-10 18:25:31 +0300

    Update dice6.py

--- dff/dice6.py
+++ dff/dice6.py
@@ -1,43 +1,16 @@
-import random
-import time
+import math
 
+a = float(input('Введите коэффициент a: '))
+b = float(input('Введите коэффициент b: '))
+c = float(input('Введите коэффициент c: '))
+d = b**2 - 4*a*c
 
-class Car:
-    def __init__(self, name, speed, distance=500) -> None:
-        self.name = name
-        self.speed = speed
-        self.distance = distance
-
-    def __del__(self):
-        print(f'{self.name} попадает в аварию и покидает гонку')
-
-    def drive(self):
-        self.distance -= self.speed
-
-    def __str__(self) -> str:
-        return f'{self.name}'
-
-
-cars = [
-    Car('BMW M5', random.randint(20, 28)),
-    Car('Lamborghini', random.randint(24, 30)),
-    Car('Honda NSX', random.randint(22, 32)),
-    Car('Chevrolet Camaro', random.randint(20, 30))
-]
-
-
-flag = True
-while flag:
-    for car in cars:
-        if random.randint(1, 10) == 1:
-            cars.remove(car)
-        else:
-            print(car)
-            print('=====================================================')
-            car.drive()
-            time.sleep(1)
-            if car.distance < 0:
-                print(f'{car} финишировалa')
-                print('=====================================================')
-                flag = False
-                break
+if d < 0:
+    print("Уравнение не имеет действительных корней")
+elif d == 0:
+    x1 = -b / (2*a)
+    print("Уравнение имеет один корень: x =", x1)
+else:
+    x1 = (-b + math.sqrt(d)) / (2*a)
+    x2 = (-b - math.sqrt(d)) / (2*a)
+    print("Уравнение имеет два корня: x1 =", x1, "x2 =", x2)
