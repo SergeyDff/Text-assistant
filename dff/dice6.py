0591e1657218d7a57fb8a7f33c9fdd3508391935
dff <130085050+SergeyDff@users.noreply.github.com>
2023-06-16 13:55:19 +0300

    Update dice6.py

--- dff/dice6.py
+++ dff/dice6.py
@@ -1,16 +1,9 @@
-import math
+nums = input('Введите числа через пробел: ').split()
+nums = [int(i) for i in nums]
+target = 9
+print(nums)
 
-a = float(input('Введите коэффициент a: '))
-b = float(input('Введите коэффициент b: '))
-c = float(input('Введите коэффициент c: '))
-d = b**2 - 4*a*c
-
-if d < 0:
-    print("Уравнение не имеет действительных корней")
-elif d == 0:
-    x1 = -b / (2*a)
-    print("Уравнение имеет один корень: x =", x1)
-else:
-    x1 = (-b + math.sqrt(d)) / (2*a)
-    x2 = (-b - math.sqrt(d)) / (2*a)
-    print("Уравнение имеет два корня: x1 =", x1, "x2 =", x2)
+for i in nums:
+    k = target - i
+    if k in nums:
+        print(nums.index(k), nums.index(i))
