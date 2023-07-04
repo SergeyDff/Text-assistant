8ae63573e6361bfb04303a0b25df917337c6e996
dff <130085050+SergeyDff@users.noreply.github.com>
2023-07-04 10:16:01 +0300

    Update dice6.py

--- dff/dice6.py
+++
@@ -1,9 +0,0 @@
-nums = input('Введите числа через пробел: ').split()
-nums = [int(i) for i in nums]
-target = 9
-print(nums)
-
-for i in nums:
-    k = target - i
-    if k in nums:
-        print(nums.index(k), nums.index(i))
